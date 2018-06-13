02106f2bbfdceed12e1539bc7815631d81bfe414
ohta2346 <y.ohta.z3@future.co.jp>
2018-06-13 18:18:40 +0900

    Add `lodash-template/attribute-name-casing` rule

---
+++ docs/rules/attribute-name-casing.md
@@ -0,0 +1,22 @@
+# enforce attribute name casing. (ex. :ok: `<div foo-bar>` :ng: `<div fooBar>` `<div FOO-BAR>`) (lodash-template/attribute-name-casing)
+
+- :gear: This rule is included in `"plugin:lodash-template/recommended-with-html"` and `"plugin:lodash-template/all"`.
+- :wrench: The `--fix` option on the [command line](http://eslint.org/docs/user-guide/command-line-interface#fix) can automatically fix some of the problems reported by this rule.
+
+## Rule Details
+
+This rule enforces attribute name casing style (kebab-case).
+
+:-1: Examples of **incorrect** code for this rule:
+
+```html
+<div fooBar="abc">
+
+<div FOO-BAR="abc">
+```
+
+:+1: Examples of **correct** code for this rule:
+
+```html
+<div foo-bar="abc">
+```
---
+++ tests/lib/rules/attribute-name-casing.js
@@ -0,0 +1,74 @@
+"use strict"
+
+
+const RuleTester = require("eslint").RuleTester
+const rule = require("../../../lib/rules/attribute-name-casing")
+
+
+const tester = new RuleTester({
+    parser: require.resolve("../../../lib/parser/micro-template-eslint-parser"),
+    parserOptions: {
+        ecmaVersion: 2015,
+    },
+})
+
+tester.run("attribute-name-casing", rule, {
+
+    valid: [
+        {
+            filename: "test.html",
+            code: "",
+        },
+        {
+            filename: "test.html",
+            code: "<body><div><div data-id=\"foo\" aria-test=\"bar\" my-prop=\"prop\"></div></div></body>",
+        },
+        {
+            filename: "test.html",
+            code: "<body><div data-id=\"foo\" aria-test=\"bar\"><a onclick=\"\" my-prop=\"prop\"></a></div></body>",
+        },
+    ],
+
+    invalid: [
+        {
+            filename: "test.html",
+            code: "<body><div><div MyProp=\"Bar\"></div></div></body>",
+            output: null,
+            errors: [{
+                message: "Attribute `MyProp` must be 'kebab-case'.",
+                type: "HTMLIdentifier",
+                line: 1,
+            }],
+        },
+        {
+            filename: "test.html",
+            code: "<body><div><div myProp=\"Bar\"></div></div></body>",
+            output: null,
+            errors: [{
+                message: "Attribute `myProp` must be 'kebab-case'.",
+                type: "HTMLIdentifier",
+                line: 1,
+            }],
+        },
+        {
+            filename: "test.html",
+            code: "<body><div><div my_prop=\"Bar\"></div></div></body>",
+            output: null,
+            errors: [{
+                message: "Attribute `my_prop` must be 'kebab-case'.",
+                type: "HTMLIdentifier",
+                line: 1,
+            }],
+        },
+        {
+            filename: "test.html",
+            code: "<body><div><div MY-PROP=\"prop\"></div></div></body>",
+            output: "<body><div><div my-prop=\"prop\"></div></div></body>",
+            errors: [{
+                message: "Attribute `MY-PROP` must be 'kebab-case'.",
+                type: "HTMLIdentifier",
+                line: 1,
+            }],
+        },
+    ],
+})
