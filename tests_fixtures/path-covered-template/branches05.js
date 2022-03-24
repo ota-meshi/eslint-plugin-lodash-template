c904fe90079751c923e616345d29fc6394cd2747
yosuke ota <otameshiyo23@gmail.com>
2022-03-24 10:52:20 +0900

    Update

---
+++ tests_fixtures/path-covered-template/branches05.js
@@ -0,0 +1,15 @@
+<% if (x > 0 && y === 42) { %>
+const a = 'x && y'
+<% } %>
+
+<% if (x <= 0) { %>
+    console.log('!x');
+<% } else { %>
+    console.log(a);
+<% } %>
+
+<% if (y!==42) { %>
+    console.log('!y');
+<% } else { %>
+    console.log(a);
+<% } %>
