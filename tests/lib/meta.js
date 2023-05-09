db8e8085f466fefe9ac649afad19abdb5050c9c2
Yosuke Ota <otameshiyo23@gmail.com>
2023-05-09 15:08:39 +0900

    feat: export meta object (#254)
    * feat: export meta object
    
    * Create loud-tomatoes-type.md

---
+++ tests/lib/meta.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const assert = require("assert")
+const plugin = require("../..")
+const expectedMeta = {
+    name: "eslint-plugin-lodash-template",
+    version: require("../../package.json").version,
+}
+
+describe("Test for meta object", () => {
+    it("A plugin should have a meta object.", () => {
+        assert.deepStrictEqual(plugin.meta, expectedMeta)
+    })
+
+    for (const [name, processor] of Object.entries(plugin.processors)) {
+        it(`"${name}" processor should have a meta object.`, () => {
+            assert.deepStrictEqual(processor.meta.version, expectedMeta.version)
+            assert.deepStrictEqual(typeof processor.meta.name, "string")
+        })
+    }
+})
