a21355c208307c398e77caf427b39f219c4b581d
ota-meshi <otameshiyo23@gmail.com>
2024-01-17 14:49:54 +0900

    chore: fix site builder

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,7 +41,6 @@ module.exports = {
     description:
         "ESLint plugin for John Resig-style micro template / Lodash's template / Underscore's template.",
     serviceWorker: true,
-    evergreen: true,
     configureWebpack(_config, _isServer) {
         return {
             resolve: {
@@ -61,9 +60,33 @@ module.exports = {
                         __dirname,
                         "../../node_modules/@eslint/eslintrc/dist/eslintrc-universal.cjs",
                     ),
+                    "eslint-compat-utils$": path.resolve(
+                        __dirname,
+                        "../../node_modules/eslint-compat-utils/dist/index.cjs",
+                    ),
                 },
             },
         };
+    },
+    chainWebpack(config) {
+        // In order to parse with webpack 4, the yaml package needs to be transpiled by babel.
+        const jsRule = config.module.rule("js");
+        const original = jsRule.exclude.values();
+        jsRule.exclude
+            .clear()
+            .add((filepath) => {
+                if (/node_modules\/(?:minimatch|yaml)\//u.test(filepath)) {
+                    return false;
+                }
+                for (const fn of original) {
+                    if (fn(filepath)) {
+                        return true;
+                    }
+                }
+                return false;
+            })
+            .end()
+            .use("babel-loader");
     },
 
     head: [["link", { rel: "icon", type: "image/png", href: "/logo.png" }]],
